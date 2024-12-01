fe5fc235f065114dac8e8178c3cd76da5c0b0853
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-12-01 16:27:51 -0500

    adds some accessability improvements to nav

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,6 +2,8 @@ const hamburger = document.querySelector(".hamburger") as HTMLElement;
 const links = document.querySelector(".nav-links") as HTMLElement;
 
 hamburger.addEventListener("click", () => {
+  const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
+  hamburger.setAttribute("aria-expanded", (!expanded).toString());
   hamburger.classList.toggle("expanded");
   links.classList.toggle("expanded");
 });
