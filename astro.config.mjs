f77b28bbf53234fd0911927e7867e1396bc9628b
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-12-03 10:53:21 -0500

    adds adapter in config and updates packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kcnq5.org",
   integrations: [sitemap()],
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
